c3f096d1d06361f25c41e3db4837b454eedfde14
양태웅 <didxodnd0422@gmail.com>
2022-04-21 20:35:11 +0900

    Test Tts

--- app/src/main/java/com/withkwon/projecttemi/ui/MainActivity.kt
+++ app/src/main/java/com/withkwon/projecttemi/ui/MainActivity.kt
@@ -3,23 +3,41 @@ package com.withkwon.projecttemi.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
 import com.robotemi.sdk.Robot
+import com.robotemi.sdk.TtsRequest
 import com.robotemi.sdk.listeners.OnRobotReadyListener
 import com.withkwon.projecttemi.R
+import com.withkwon.projecttemi.databinding.ActivityMainBinding
 import com.withkwon.projecttemi.util.Constants.TAG
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(), OnRobotReadyListener {
+    private val binding: ActivityMainBinding by lazy { DataBindingUtil.setContentView(this, R.layout.activity_main) }
     private val mainViewModel: MainViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) = with(binding) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        lifecycleOwner = this@MainActivity
 
-        mainViewModel.robot.addOnRobotReadyListener(this)
+        mainViewModel.robot.addOnRobotReadyListener(this@MainActivity)
+        buttonTest.setOnClickListener {
+            editText.text.toString().let {
+                if (it.isBlank()) {
+                    Toast.makeText(this@MainActivity, "칸을 입력해주세요", Toast.LENGTH_SHORT).show()
+                } else {
+                    mainViewModel.speak(
+                        speech = it,
+                        isShowOnConversationLayer = false,
+                        language = TtsRequest.Language.KO_KR
+                    )
+                }
+            }
+        }
     }
 
     override fun onRobotReady(isReady: Boolean) {
--- app/src/main/java/com/withkwon/projecttemi/ui/MainViewModel.kt
+++ app/src/main/java/com/withkwon/projecttemi/ui/MainViewModel.kt
@@ -2,6 +2,7 @@ package com.withkwon.projecttemi.ui
 
 import androidx.lifecycle.ViewModel
 import com.robotemi.sdk.Robot
+import com.robotemi.sdk.TtsRequest
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -9,5 +10,12 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(
     val robot: Robot
 ) : ViewModel() {
-
+    fun speak(speech: String, isShowOnConversationLayer: Boolean, language: TtsRequest.Language) {
+        val ttsRequest = TtsRequest.create(
+            speech = speech,
+            isShowOnConversationLayer = isShowOnConversationLayer,
+            language = language
+        )
+        robot.speak(ttsRequest)
+    }
 }
