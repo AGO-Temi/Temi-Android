4984c7f7693b4f46b78a8c3da977a923ba814e3c
KDY <rlaeodud9983@naver.com>
2022-04-25 20:37:00 +0900

    [UPDATE] MainFragment Layout 작성, 각 UI누를시 UI에 해당하는 페이지로 이동 제작

--- app/src/main/java/com/withkwon/projecttemi/ui/fragment/MainFragment.kt
+++ app/src/main/java/com/withkwon/projecttemi/ui/fragment/MainFragment.kt
@@ -6,27 +6,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.withkwon.projecttemi.R
+import com.withkwon.projecttemi.databinding.MainFragmentBinding
+import dagger.hilt.android.AndroidEntryPoint
 
 class MainFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = MainFragment()
-    }
 
-    private lateinit var viewModel: MainViewModel
+    private val viewModel: MainViewModel by viewModels()
+    private val binding by lazy { MainFragmentBinding.inflate(layoutInflater) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        binding.fragment = this
+        binding.lifecycleOwner= this
+
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+    fun goSchoolIntroduce(){
+        findNavController().navigate(R.id.action_mainFragment_to_introduceSchoolFragment)
+    }
+
+    fun goDirectionGuidance(){
+        findNavController().navigate(R.id.action_mainFragment_to_placeListFragment)
     }
 
 }
