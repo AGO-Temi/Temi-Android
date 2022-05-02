6eb7819015a134cfc0cd94f98dd4cc7ef1f4cf87
KDY <rlaeodud9983@naver.com>
2022-05-02 20:00:59 +0900

    [UPDATE] PlayList병합

--- app/src/main/java/com/withkwon/projecttemi/ui/fragment/MovingViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.withkwon.projecttemi.ui.fragment
-
-import androidx.lifecycle.ViewModel
-
-class MovingViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/com/withkwon/projecttemi/ui/fragment/PlaceDescriptionViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.withkwon.projecttemi.ui.fragment
-
-import androidx.lifecycle.ViewModel
-
-class PlaceDescriptionViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
