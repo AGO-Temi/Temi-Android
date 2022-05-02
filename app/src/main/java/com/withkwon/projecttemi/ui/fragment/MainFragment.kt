8deef28458f7b55860d2e284a525cd435b0d06bb
KDY <rlaeodud9983@naver.com>
2022-05-02 19:44:41 +0900

    [ADD] 각 페이지 가는 함수 추가

--- app/src/main/java/com/withkwon/projecttemi/ui/fragment/MainFragment.kt
+++ app/src/main/java/com/withkwon/projecttemi/ui/fragment/MainFragment.kt
@@ -1,6 +1,5 @@
 package com.withkwon.projecttemi.ui.fragment
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -12,8 +11,8 @@ import com.withkwon.projecttemi.R
 import com.withkwon.projecttemi.databinding.MainFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainFragment : Fragment() {
-
 
     private val viewModel: MainViewModel by viewModels()
     private val binding by lazy { MainFragmentBinding.inflate(layoutInflater) }
@@ -24,18 +23,24 @@ class MainFragment : Fragment() {
     ): View? {
         binding.fragment = this
         binding.lifecycleOwner= this
+        //KioskMode
+        viewModel.robot.requestToBeKioskApp()
+        viewModel.robot.setKioskModeOn()
 
         return binding.root
     }
 
+    //학교소개
     fun goSchoolIntroduce(){
         findNavController().navigate(R.id.action_mainFragment_to_introduceSchoolFragment)
     }
 
+    //길안내
     fun goDirectionGuidance(){
         findNavController().navigate(R.id.action_mainFragment_to_placeListFragment)
     }
 
+    //크레딧
     fun goCredit(){
         findNavController().navigate(R.id.action_mainFragment_to_creditFragment)
     }
