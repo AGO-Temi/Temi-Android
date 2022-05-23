30efb3f55197b045de9f292247cd183bd231fec0
양태웅 <didxodnd0422@gmail.com>
2022-05-23 20:48:39 +0900

    [ADD] Add back button

--- app/src/main/java/com/withkwon/projecttemi/ui/fragment/IntroduceSchoolFragment.kt
+++ app/src/main/java/com/withkwon/projecttemi/ui/fragment/IntroduceSchoolFragment.kt
@@ -1,21 +1,38 @@
 package com.withkwon.projecttemi.ui.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import com.withkwon.projecttemi.R
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.withkwon.projecttemi.databinding.IntroduceSchoolFragmentBinding
+import com.withkwon.projecttemi.util.Constants.TAG
 
 class IntroduceSchoolFragment : Fragment() {
     private val binding by lazy { IntroduceSchoolFragmentBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding.buttonBack.setOnClickListener {
+            Log.d(TAG, "onCreateView: ${findNavController().navigateUp()}")
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? = with(binding) {
         webView.loadUrl("http://gsm.gen.hs.kr/sub/page.php?page_code=info_01")
+        webView.setInitialScale(1)
+        webView.settings.apply {
+            useWideViewPort = true
+            loadWithOverviewMode = true
+            builtInZoomControls = true
+            setSupportZoom(true)
+        }
 
         return root
     }
