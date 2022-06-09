0b00abcd43ad7bcce920a770082f3711dfb4ea12
KDY <rlaeodud9983@naver.com>
2022-06-09 20:59:32 +0900

    [UPDATE] 사용하지 않는 코드 삭제

--- app/src/main/java/com/withkwon/projecttemi/ui/activity/MainActivity.kt
+++ app/src/main/java/com/withkwon/projecttemi/ui/activity/MainActivity.kt
@@ -12,7 +12,6 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import com.robotemi.sdk.listeners.OnRobotReadyListener
 import com.withkwon.projecttemi.R
 import com.withkwon.projecttemi.databinding.ActivityMainBinding
 import com.withkwon.projecttemi.ui.fragment.MainViewModel
@@ -22,7 +21,6 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(){
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-    private val mainViewModel: MainViewModel by viewModels()
     private val navController by lazy { findNavController(R.id.nav_host_fragment) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
